03fc6ee529b12fdc9b3e0a8c409985408977bab4
Chris Keavey <chris@chriskeavey.com>
2015-12-11 20:13:52 +0000

    Add code coment

--- src/index.php
+++ src/index.php
@@ -87,7 +87,7 @@
                   $x = 0;
                   foreach ($events as $key => $value) {
                     $eventend = strtotime($key)+86399; /* Last date of event (which is 1 second into that date i.e. on the MORNING of the date) plus 23 hours 59 minutes to make it the end of that day */
-                    if (($now <= $eventend) && ($x < 3)) {
+                    if (($now <= $eventend) && ($x < 3)) { // Limit to only next 3 events
                       $x++;
                       $eventid = 'event'.$x; ?>
                 <!--START EVENT -->
