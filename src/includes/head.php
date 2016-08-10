8442b5e6c0d671772c311b84d005ad81aa66fdd3
Chris Keavey <chris@chriskeavey.com>
2016-08-10 19:20:29 +0100

    Fix IOS display to allow fit to screen (zoom out)

--- src/includes/head.php
+++ src/includes/head.php
@@ -1,7 +1,8 @@
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 
-<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, minimal-ui">
-<meta name="apple-mobile-web-app-capable" content="yes">
+<!-- <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, minimal-ui"> -->
+<meta name="viewport" content="width=device-width">
+<!-- <meta name="apple-mobile-web-app-capable" content="yes"> -->
 
 <link href='http://fonts.googleapis.com/css?family=Gentium+Basic:400,400italic,700,700italic|PT+Sans:400,400italic,700,700italic' rel='stylesheet' type='text/css'/>
 
