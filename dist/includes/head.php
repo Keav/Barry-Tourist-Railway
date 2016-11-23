e8e58a4c932f6d883591bcbd5bae3d7cee52f464
Chris Keavey <chris@chriskeavey.com>
2016-11-23 21:51:58 +0000

    Grunt Build

--- dist/includes/head.php
+++ dist/includes/head.php
@@ -1,11 +1,11 @@
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 
-<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, minimal-ui">
+<meta name="viewport" content="width=1040, user-scalable=yes">
 <meta name="apple-mobile-web-app-capable" content="yes">
 
 <link href='http://fonts.googleapis.com/css?family=Gentium+Basic:400,400italic,700,700italic|PT+Sans:400,400italic,700,700italic' rel='stylesheet' type='text/css'/>
 
 <link rel="stylesheet" type="text/css" href="css/grid.min.css" />
-<link rel="stylesheet" type="text/css" href="css/main.min.f14195d5.css" />
+<link rel="stylesheet" type="text/css" href="css/main.min.bd7a5094.css" />
 
 <link rel="stylesheet" href="font-awesome-4.2.0/css/font-awesome.min.css">
