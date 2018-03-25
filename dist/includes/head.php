3f0697bdc7dd9744511c25b2eb71008978601b2b
Chris Keavey <chris@chriskeavey.com>
2018-03-25 12:50:29 +0100

    Grunt build

--- dist/includes/head.php
+++ dist/includes/head.php
@@ -6,6 +6,6 @@
 <link href='http://fonts.googleapis.com/css?family=Gentium+Basic:400,400italic,700,700italic|PT+Sans:400,400italic,700,700italic' rel='stylesheet' type='text/css'/>
 
 <link rel="stylesheet" type="text/css" href="css/grid.min.css" />
-<link rel="stylesheet" type="text/css" href="css/main.min.bd7a5094.css" />
+<link rel="stylesheet" type="text/css" href="css/main.min.8ec19895.css" />
 
 <link rel="stylesheet" href="font-awesome-4.2.0/css/font-awesome.min.css">
